83d16747954a08a30f9ab3b6abb04ffde7499602
Blackmore5 <tonianev@gmail.com>
2017-01-27 00:12:28 -0500

    Update spotFinder.py

--- spotFinder.py
+++ spotFinder.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 import time
 
-
 chrome_path = r"C:\Program Files (x86)\Google\Chrome\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
 driver.get("https://reservations.ontarioparks.com/OntarioParks?Map")
@@ -18,7 +17,6 @@ unavailSites = driver.find_elements_by_class_name("area_unavail")
 def checking():
 	if availSites:
 		print("We've got something here.")
-
 	else:
 		print("Still nothing...")
 
